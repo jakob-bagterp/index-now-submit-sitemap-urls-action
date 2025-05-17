71e71aafb8977d13dea982eb45b9855c78d7713e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-17 13:18:01 +0200

    Fixed minor missing return type.

--- test/helper/index_now/api_key_generator_test.py
+++ test/helper/index_now/api_key_generator_test.py
@@ -3,7 +3,7 @@ import re
 from helper.index_now.generate_api_key import generate_random_api_key
 
 
-def test_generate_random_api_key():
+def test_generate_random_api_key() -> None:
     api_key_pattern = re.compile(r"^[0-9a-f]{32}$")
     for _ in range(100):
         api_key = generate_random_api_key()
