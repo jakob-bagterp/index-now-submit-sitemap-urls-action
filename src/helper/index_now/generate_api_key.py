eedc5f3028ae62bdd6747d09df269116801dfc8b
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-17 13:02:44 +0200

    Minor update to docstring.

--- src/helper/index_now/generate_api_key.py
+++ src/helper/index_now/generate_api_key.py
@@ -2,7 +2,11 @@ import uuid
 
 
 def generate_random_api_key() -> str:
-    """Generate an API key for IndexNow, e.g. `5017988d51af458491d21ecab6ed1811`."""
+    """Generate an API key for IndexNow.
+
+    Returns:
+        str: A 32 character hexadecimal string, e.g. `5017988d51af458491d21ecab6ed1811`.
+    """
 
     return str(uuid.uuid4()).replace("-", "")[:32]
 
