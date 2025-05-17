60cd4c1278c2841f9380468c10a34c054401734d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-17 20:04:17 +0200

    Minor renamed method.

--- test/helper/repository/add_and_remove_api_key_file_test.py
+++ test/helper/repository/add_and_remove_api_key_file_test.py
@@ -10,7 +10,7 @@ from helper.repository.remove_api_key_file import remove_api_key_file
 
 
 def test_add_and_remove_api_key_file() -> None:
-    def get_name_of_current_branch() -> str:
+    def get_name_of_current_git_branch() -> str:
         return subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"], text=True).strip()
 
     def clean_up_and_remove_latest_commits(commit_count: int, return_to_branch: str) -> None:
@@ -33,7 +33,7 @@ def test_add_and_remove_api_key_file() -> None:
 
     api_key = "a1b2c3d4"
     api_key_file_name = get_api_key_file_name(api_key)
-    origin_branch = get_name_of_current_branch()
+    origin_branch = get_name_of_current_git_branch()
 
     create_api_key_file(api_key)
     assert os.path.exists(api_key_file_name)
