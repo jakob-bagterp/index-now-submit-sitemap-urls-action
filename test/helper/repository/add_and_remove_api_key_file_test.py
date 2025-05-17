b6ea64aa292091b577d858985bbfb5eddfe7f8ff
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-17 19:41:11 +0200

    Added extra tests and more robustness.

--- test/helper/repository/add_and_remove_api_key_file_test.py
+++ test/helper/repository/add_and_remove_api_key_file_test.py
@@ -4,6 +4,7 @@ import subprocess
 from helper.index_now.api_key import get_api_key_file_name
 from helper.repository.add_api_key_file import create_api_key_file
 from helper.repository.remove_api_key_file import remove_api_key_file
+from helper.repository.shared import GH_PAGES_BRANCH_NAME, go_to_branch
 
 
 def test_add_and_remove_api_key_file() -> None:
@@ -11,16 +12,20 @@ def test_add_and_remove_api_key_file() -> None:
         return subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"], text=True).strip()
 
     def clean_up_and_remove_latest_commits(commit_count: int, return_to_branch: str) -> None:
+        go_to_branch(GH_PAGES_BRANCH_NAME)
         subprocess.run(["git", "reset", "--hard", f"HEAD~{commit_count}"])
         subprocess.run(["git", "checkout", return_to_branch])
 
-    origin_branch = get_name_of_current_branch()
-
     api_key = "a1b2c3d4"
     api_key_file_name = get_api_key_file_name(api_key)
+    origin_branch = get_name_of_current_branch()
+
     create_api_key_file(api_key)
     assert os.path.exists(api_key_file_name)
     assert os.path.isfile(api_key_file_name)
+    with open(api_key_file_name) as file:
+        assert file.read().strip() == api_key
+
     remove_api_key_file(api_key)
     assert not os.path.exists(api_key_file_name)
 
