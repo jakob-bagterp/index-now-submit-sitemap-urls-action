43cea5db14bf9f3e2c4db7a04963ef99bff1e8da
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-17 14:11:20 +0200

    Fixed grammar issue in previous commit.

--- test/helper/index_now/get_submit_sitemap_cli_input_parameters_test.py
+++ test/helper/index_now/get_submit_sitemap_cli_input_parameters_test.py
@@ -1,11 +1,11 @@
 from helper.index_now.submit_sitemap import \
-    get_submit_sitempa_cli_input_parameters
+    get_submit_sitemap_cli_input_parameters
 
 
-def test_get_submit_sitempa_cli_input_parameters() -> None:
+def test_get_submit_sitemap_cli_input_parameters() -> None:
     command = "submit_sitemap.py example.com a1b2c3d4 https://example.com/a1b2c3d4.txt https://example.com/sitemap.xml yandex"
     command_parameters = [str(paramater) for paramater in command.split(" ")]
-    input = get_submit_sitempa_cli_input_parameters(command_parameters)
+    input = get_submit_sitemap_cli_input_parameters(command_parameters)
     assert input.host == "example.com"
     assert input.api_key == "a1b2c3d4"
     assert input.api_key_location == "https://example.com/a1b2c3d4.txt"
