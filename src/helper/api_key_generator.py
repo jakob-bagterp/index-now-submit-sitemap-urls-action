82154715bb1d39610eaa7905665e0392c42d0bc8
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-17 07:53:59 +0200

    Added key generator, including tests.

---
+++ src/helper/api_key_generator.py
@@ -0,0 +1,11 @@
+import uuid
+
+
+def generate_random_api_key() -> str:
+    """Generate an API key for IndexNow, e.g. `5017988d51af458491d21ecab6ed1811`."""
+
+    return str(uuid.uuid4()).replace("-", "")[:32]
+
+
+if __name__ == "__main__":
+    print(generate_random_api_key())
---
+++ test/api_key_generator_test.py
@@ -0,0 +1,10 @@
+import re
+
+from helper.api_key_generator import generate_random_api_key
+
+
+def test_generate_random_api_key():
+    api_key_pattern = re.compile(r"^[0-9a-f]{32}$")
+    for _ in range(100):
+        api_key = generate_random_api_key()
+        assert api_key_pattern.match(api_key)
