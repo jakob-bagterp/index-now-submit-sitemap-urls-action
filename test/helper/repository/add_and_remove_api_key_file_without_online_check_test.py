4bdba6e58858eed141f1c89c8af2a8adbc16f56e
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-19 22:23:43 +0200

    Minor fix of handling of origin branch.

--- test/helper/repository/add_and_remove_api_key_file_without_online_check_test.py
+++ test/helper/repository/add_and_remove_api_key_file_without_online_check_test.py
@@ -6,14 +6,12 @@ from shared import (ORIGIN_BRANCH_NAME,
 from helper.index_now.api_key import get_api_key_file_name
 from helper.repository.add_api_key_file import create_api_key_file
 from helper.repository.remove_api_key_file import remove_api_key_file
-from helper.repository.shared import (get_name_of_current_git_branch,
-                                      go_to_branch)
+from helper.repository.shared import go_to_branch
 
 
 def test_add_and_remove_api_key_file_without_online_check() -> None:
     api_key = "a1b2c3d4"
     api_key_file_name = get_api_key_file_name(api_key)
-    origin_branch = get_name_of_current_git_branch()
 
     go_to_branch(ORIGIN_BRANCH_NAME)  # Reset to the origin branch before running the test.
     assert "sitemap.xml" not in os.listdir()  # Ensure that we're not in the root of GitHub Pages where the sitemap file is.
@@ -30,4 +28,4 @@ def test_add_and_remove_api_key_file_without_online_check() -> None:
     assert not os.path.exists(api_key_file_name)
 
     clean_up_and_remove_latest_commits_from_gh_pages(commit_count=2, verify_commit_messages=[added_api_key_commit_message, removed_api_key_commit_message])
-    go_to_branch(origin_branch)
+    go_to_branch(ORIGIN_BRANCH_NAME)
