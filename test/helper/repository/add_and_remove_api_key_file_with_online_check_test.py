684c3f81e61318012677eaddb1a16a70c71a0896
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-19 23:00:17 +0200

    Another attempt to fix branch switch issue by using "git switch -" command.

--- test/helper/repository/add_and_remove_api_key_file_with_online_check_test.py
+++ test/helper/repository/add_and_remove_api_key_file_with_online_check_test.py
@@ -2,13 +2,13 @@ import os
 
 import pytest
 from shared import (attempt_to_get_api_key_file_from_gh_pages,
-                    clean_up_and_remove_latest_commits_from_gh_pages)
+                    clean_up_and_remove_latest_commits_from_gh_pages,
+                    go_to_origin_branch)
 
 from helper.index_now.api_key import get_api_key_file_name
 from helper.repository.add_api_key_file import create_api_key_file
 from helper.repository.remove_api_key_file import remove_api_key_file
-from helper.repository.shared import (GITHUB_WORKSPACE_TOKEN,
-                                      ORIGIN_BRANCH_NAME, go_to_branch)
+from helper.repository.shared import GITHUB_WORKSPACE_TOKEN
 
 
 def test_add_and_remove_api_key_file_with_online_check() -> None:
@@ -18,7 +18,7 @@ def test_add_and_remove_api_key_file_with_online_check() -> None:
     api_key = "a1b2c3d4"
     api_key_file_name = get_api_key_file_name(api_key)
 
-    go_to_branch(ORIGIN_BRANCH_NAME)  # Reset to the origin branch before running the test.
+    go_to_origin_branch()  # Reset to the origin branch before running the test.
     assert "sitemap.xml" not in os.listdir()  # Ensure that we're not in the root of GitHub Pages where the sitemap file is.
 
     added_api_key_commit_message = "TEST: Added API key file"
@@ -39,4 +39,4 @@ def test_add_and_remove_api_key_file_with_online_check() -> None:
     assert not os.path.exists(api_key_file_name)
 
     clean_up_and_remove_latest_commits_from_gh_pages(commit_count=2, verify_commit_messages=[added_api_key_commit_message, removed_api_key_commit_message])
-    go_to_branch(ORIGIN_BRANCH_NAME)
+    go_to_origin_branch()
