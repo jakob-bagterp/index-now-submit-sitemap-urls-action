251ba55e7d3f92187921735fabde80579656edbe
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-25 15:09:26 +0200

    Added URLs as optional input and refactored sitemap locations to optional, including updated tests.

---
+++ test/helper/parse_urls_test.py
@@ -0,0 +1,23 @@
+import pytest
+
+from helper.submit_sitemap import parse_string_or_list_input
+
+EXPECTED_URL = ["https://example.com"]
+EXPECTED_URLS = ["https://example.com/page1", "https://example.com/page2"]
+
+
+@pytest.mark.parametrize("sitemap_locations, expected", [
+    ("", []),
+    (None, []),
+    ("https://example.com", EXPECTED_URL),
+    ('https://example.com', EXPECTED_URL),
+    ("['https://example.com/page1', 'https://example.com/page2']", EXPECTED_URLS),
+    ('["https://example.com/page1", "https://example.com/page2"]', EXPECTED_URLS),
+    ("[\"https://example.com/page1\", \"https://example.com/page2\"]", EXPECTED_URLS),
+    ('[\'https://example.com/page1\', \'https://example.com/page2\']', EXPECTED_URLS),
+    ("[ 'https://example.com/page1', ' https://example.com/page2 ' ]", EXPECTED_URLS),
+    ('[ "https://example.com/page1", " https://example.com/page2 "]', EXPECTED_URLS),
+    ("https://example.com/page1, https://example.com/page2", EXPECTED_URLS),
+])
+def test_parse_urls_input(sitemap_locations: str, expected: list[str]) -> None:
+    assert parse_string_or_list_input(sitemap_locations) == expected
