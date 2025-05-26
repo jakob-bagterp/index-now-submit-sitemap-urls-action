fc17a9e972a0cb12fcdfaa1a59c999f1f01dbdf7
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-26 21:30:01 +0200

    Added tests of returned exit codes from subprocess.call() method.

--- test/helper/submit/nothing_test.py
+++ test/helper/submit/nothing_test.py
@@ -1,15 +1,18 @@
 import subprocess
 
+from constant import SUCCESS_EXIT_CODE
+
 
 def test_submit_nothing_from_terminal(capfd: object) -> None:
-    subprocess.call(["python3", "./src/helper/submit.py",
-                     "jakob-bagterp.github.io",
-                     "6d71a14ac15c4c41a0c19e641f659208",
-                     "https://jakob-bagterp.github.io/index-now-api-key.txt",
-                     "yandex",
-                     "--sitemap-locations", "",
-                     "--urls", "",
-                     ])
+    status_code = subprocess.call(["python3", "./src/helper/submit.py",
+                                   "jakob-bagterp.github.io",
+                                   "6d71a14ac15c4c41a0c19e641f659208",
+                                   "https://jakob-bagterp.github.io/index-now-api-key.txt",
+                                   "yandex",
+                                   "--sitemap-locations", "",
+                                   "--urls", "",
+                                   ])
+    assert status_code == SUCCESS_EXIT_CODE
     terminal_output, _ = capfd.readouterr()
     assert "No sitemaps to submit. Skipping..." in terminal_output
     assert "No URLs to submit. Skipping..." in terminal_output
--- test/helper/submit/sitemaps_test.py
+++ test/helper/submit/sitemaps_test.py
@@ -2,6 +2,7 @@ import subprocess
 
 import pytest
 from colorist import Color
+from constant import SUCCESS_EXIT_CODE
 
 
 @pytest.mark.parametrize("sitemap_locations", [
@@ -9,14 +10,15 @@ from colorist import Color
     "https://jakob-bagterp.github.io/sitemap.xml, https://jakob-bagterp.github.io/index-now-submit-sitemap-urls-action/sitemap.xml"
 ])
 def test_submit_sitemaps_from_terminal(sitemap_locations: str, capfd: object) -> None:
-    subprocess.call(["python3", "./src/helper/submit.py",
-                     "jakob-bagterp.github.io",
-                     "6d71a14ac15c4c41a0c19e641f659208",
-                     "https://jakob-bagterp.github.io/index-now-api-key.txt",
-                     "yandex",
-                     "--sitemap-locations", sitemap_locations,
-                     "--urls", "",
-                     ])
+    status_code = subprocess.call(["python3", "./src/helper/submit.py",
+                                   "jakob-bagterp.github.io",
+                                   "6d71a14ac15c4c41a0c19e641f659208",
+                                   "https://jakob-bagterp.github.io/index-now-api-key.txt",
+                                   "yandex",
+                                   "--sitemap-locations", sitemap_locations,
+                                   "--urls", "",
+                                   ])
+    assert status_code == SUCCESS_EXIT_CODE
     terminal_output, _ = capfd.readouterr()
     assert f"URL(s) submitted successfully to the IndexNow API:{Color.OFF}" in terminal_output
     assert f"Status code: {Color.GREEN}200{Color.OFF}" or f"Status code: {Color.GREEN}202{Color.OFF}" in terminal_output
