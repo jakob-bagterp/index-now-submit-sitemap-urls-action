306597cea8ba69bbffd0de1298bf47856470c0e5
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-26 21:25:05 +0200

    Extended failure test and ensure that we also receive an error message when using the same submit method with the same parameters.

--- test/helper/submit/failure_test.py
+++ test/helper/submit/failure_test.py
@@ -1,27 +1,52 @@
 import subprocess
 
 from constant import EXIT_CODE_FAILURE
+from index_now import (IndexNowAuthentication, SearchEngineEndpoint,
+                       submit_sitemaps_to_index_now, submit_urls_to_index_now)
+
+from helper.result import SUCCESS_STATUS_CODES
+
+VALID_HOST = "jakob-bagterp.github.io"
+INVALID_API_KEY = "invalid"
+INVALID_API_KEY_LOCATION = "https://jakob-bagterp.github.io/invalid.txt"
+
+AUTHENTICATION = IndexNowAuthentication(
+    host=VALID_HOST,
+    api_key=INVALID_API_KEY,
+    api_key_location=INVALID_API_KEY_LOCATION
+)
+
+SITEMAP_LOCATIONS = ["https://jakob-bagterp.github.io/sitemap.xml"]
+URLS = ["https://jakob-bagterp.github.io"]
 
 
 def test_submit_sitemaps_from_terminal_failure() -> None:
+    # Ensure that we also receive an error message when using the same submit method and parameters:
+    status_code = submit_sitemaps_to_index_now(AUTHENTICATION, SITEMAP_LOCATIONS, endpoint=SearchEngineEndpoint.BING)
+    assert status_code not in SUCCESS_STATUS_CODES
+
     exit_code = subprocess.call(["python3", "./src/helper/submit.py",
-                                 "jakob-bagterp.github.io",
-                                 "invalid",
-                                 "https://jakob-bagterp.github.io/invalid.txt",
+                                 VALID_HOST,
+                                 INVALID_API_KEY,
+                                 INVALID_API_KEY_LOCATION,
                                  "bing",
-                                 "--sitemap-locations", "https://jakob-bagterp.github.io/sitemap.xml",
+                                 "--sitemap-locations", SITEMAP_LOCATIONS[0],
                                  "--urls", "",
                                  ])
     assert exit_code == EXIT_CODE_FAILURE
 
 
 def test_submit_urls_from_terminal_failure() -> None:
+    # Ensure that we also receive an error message when using the same submit method and parameters:
+    status_code = submit_urls_to_index_now(AUTHENTICATION, URLS, endpoint=SearchEngineEndpoint.BING)
+    assert status_code not in SUCCESS_STATUS_CODES
+
     exit_code = subprocess.call(["python3", "./src/helper/submit.py",
-                                 "jakob-bagterp.github.io",
-                                 "invalid",
-                                 "https://jakob-bagterp.github.io/invalid.txt",
+                                 VALID_HOST,
+                                 INVALID_API_KEY,
+                                 INVALID_API_KEY_LOCATION,
                                  "bing",
                                  "--sitemap-locations", "",
-                                 "--urls", "https://jakob-bagterp.github.io",
+                                 "--urls", URLS[0],
                                  ])
     assert exit_code == EXIT_CODE_FAILURE
