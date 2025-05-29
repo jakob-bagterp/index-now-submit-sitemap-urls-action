9d378838f22dab3cc1d78aced45a9e0f695e2967
Jakob Bagterp <jakob_bagterp@hotmail.com>
2025-05-29 23:09:08 +0200

    Minor fixed brackets.

--- test/helper/parse/sitemap_filter_test.py
+++ test/helper/parse/sitemap_filter_test.py
@@ -15,8 +15,7 @@ from helper.submit import parse_sitemap_filter_input
     ('\"section1\"', "section1"),
     ("r'(section1|section2)'", r"(section1|section2)"),
     ('r"(section1|section2)"', r"(section1|section2)"),
-]
-)
+])
 def test_parse_sitemap_filter_input(sitemap_filter_input: str, expected: str | None) -> None:
     result = parse_sitemap_filter_input(sitemap_filter_input)
     assert result == expected
@@ -35,8 +34,7 @@ def test_parse_sitemap_filter_input(sitemap_filter_input: str, expected: str | N
     (SITEMAP_URLS, r"(section1|section3)", ["https://example.com/section1/page1"]),
     (SITEMAP_URLS, "r'(section1|section2)'", URLS),
     (SITEMAP_URLS, 'r"(section1|section2)"', URLS),
-]
-)
+])
 def test_filter_urls(sitemap_urls: list[SitemapUrl], sitemap_filter_input: str, expected: list[str]) -> None:
     contains = parse_sitemap_filter_input(sitemap_filter_input)
     filter = SitemapFilter(contains=contains)
